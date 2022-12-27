f9c0281a16dca9c0418b88ddf55675149902330c
mac <93315891+technozhantomz@users.noreply.github.com>
2022-12-27 14:57:30 +0300

    -

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,12 @@
-import path from 'path';
-import webpack from 'webpack';
+const path = require('path');
+const webpack = require('webpack');
 
-const config = {
+const config = module.exports = {
     mode: "production",
+    entry: path.resolve('./src/index.js'),
     output: {
-      path: path.resolve('./dist'),
-      filename: 'beet-js.js',
-    },
-    optimization: {
-      minimize: true,
-      minimizer: [() => ({ terserOptions: { mangle: false } })]
+        path: path.resolve('./dist'),
+        filename: 'beet-js.js', //ignored?
     },
     profile: true
 };
-
-export default config;
